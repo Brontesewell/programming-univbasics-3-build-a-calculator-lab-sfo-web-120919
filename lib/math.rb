021e3c445882093f5b1ccfb3a768eaa2c19bcf38
Bronte Sewell <brontsewell7@gmail.com>
2019-11-26 23:19:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,17 +8,17 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  puts num2 / num1
 end
 
 def multiplication(num1, num2)
-
+  puts num2 * num1
 end
 
 def modulo(num1, num2)
-
+  puts num2 % num1
 end
 
 def square_root(num)
-
+  puts num2  num1
 end
